357a5248a01dd71c82d2b45d33c3595fee1d56c7
Vivek kumar srivastava <viveksrivastava045@gmail.com>
2026-07-10 23:06:42 +0530

    Update app.py

--- PROJECT_04_HR_comma/app.py
+++ PROJECT_04_HR_comma/app.py
@@ -22,7 +22,7 @@ st.write("Predict whether an employee will leave the company.")
 
 @st.cache_data
 def load_data():
-    return pd.read_csv("HR_comma_sep.csv")
+    return pd.read_csv("PROJECT_04_HR_comma/HR_comma_sep.csv")
 
 df = load_data()
 
