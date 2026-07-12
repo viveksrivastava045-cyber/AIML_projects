14740fee7b80ab6d222f5c857eea7dba738396fe
Vivek Kumar Srivastava <viveksrivastava045@gmail.com>
2026-07-12 23:41:52 +0530

    Update app.py

--- PROJECT_05_Male_female/app.py
+++ PROJECT_05_Male_female/app.py
@@ -31,7 +31,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     # Display the uploaded image
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=False)
     
     # Process button
     if st.button("Predict Gender"):
