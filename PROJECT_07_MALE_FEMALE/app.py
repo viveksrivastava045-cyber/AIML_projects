e327a16c08cc3b9cf64b1af032aa4ad9a11355fe
Vivek Kumar Srivastava <viveksrivastava045@gmail.com>
2026-07-13 14:39:25 +0530

    Update app.py

--- PROJECT_07_MALE_FEMALE/app.py
+++ PROJECT_07_MALE_FEMALE/app.py
@@ -32,7 +32,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     # Display the uploaded image
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', width = 64)
+    st.image(image, caption='Uploaded Image', width = 128)
     st.write("")
     
     with st.spinner('Analyzing image...'):
